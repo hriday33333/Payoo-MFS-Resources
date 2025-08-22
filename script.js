0f91ff0d840081a319b046749d72bbe9ce36e29d
hriday333 <zakirhasanhriday@gmaik.com>
2025-08-22 21:38:28 +0600

    added js file login ststem and html file

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // loging btn
 document.getElementById('login-btn').addEventListener('click', function(e){
     e.preventDefault()
-    console.log('loging btn this ')
-    console.log(e)
+    const mobileNumber = 1234567810
+    const pinNumber = 1234
+    const mobileNumberValue = document.getElementById('mobile-number').value
+    const mobileNumberValueConbert = parseInt (mobileNumberValue)
+    const pinNumberValue = document.getElementById('pin-number').value
+    const pinNumberValueConbert = parseInt(pinNumberValue)
+
+    if(mobileNumberValueConbert===mobileNumber && pinNumberValueConbert===pinNumber){
+       window.location.href="./main.html"
+    }
+    else{
+        alert('invalid credentials')
+    }
 })
